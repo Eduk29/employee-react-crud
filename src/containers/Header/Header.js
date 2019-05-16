bd17c635e004f660424f248f2bb1de59c4077c27
Eduardo Marques <jose.eduardo.marques@accenture.com>
2019-05-16 18:29:39 -0300

    Impl. Header container

---
+++ src/containers/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Container Header</h1>
+      <h2>TopNavabar Component</h2>
+      <h2>Masthead Component</h2>
+    </div>
+  )
+}
+
+export default Header;
