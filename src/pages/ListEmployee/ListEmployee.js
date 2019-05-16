37dfc012a37ffcf5fab48201e05cccceba94a749
Eduardo Marques <jose.eduardo.marques@accenture.com>
2019-05-16 18:40:09 -0300

    Add Header to ListEmployee Container

--- src/pages/ListEmployee/ListEmployee.js
+++ src/pages/ListEmployee/ListEmployee.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
+import Header from "../../containers/Header/Header";
 
 const ListEmployee = () => {
   return (
     <div>
       <h1>Page ListEmployee</h1>
+      <Header />
     </div>
-  )
-}
+  );
+};
 
 export default ListEmployee;
