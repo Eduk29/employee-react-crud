8fe00cebdc68986600f21cb188d6493419b24828
Eduardo Marques <jose.eduardo.marques@accenture.com>
2019-05-17 15:38:45 -0300

    Adjusts in Header to impl. TopMenu and Masthead

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Masthead from "../../components/Masthead";
+import TopMenu from "../../components/TopMenu";
 
 const Header = () => {
+
   return (
     <div>
-      <h1>Container Header</h1>
-      <h2>TopNavabar Component</h2>
-      <Masthead title='Masthead Component Title' class='d-flex flex-row justify-content-center align-items-center masthead masthead-background' />
+      {/* <h1>Container Header</h1> */}
+      <TopMenu title="TopMenu Component Title" />
+      <Masthead title="React CRUD Example App" class='d-flex flex-row justify-content-center align-items-center masthead masthead-background' />
     </div>
   );
 };
