410d22d3aa62214a28d905d69871cfc34be03360
Eduardo Marques <jose.eduardo.marques@accenture.com>
2019-05-17 16:31:37 -0300

    Creation and implementation of List Container

---
+++ src/containers/List/List.js
@@ -0,0 +1,13 @@
+import React from "react";
+import EmployeeTable from "../../components/EmployeeTable/EmployeeTable";
+
+const List = () => {
+  return (
+    <div className="container">
+      <h2>Container List</h2>
+      <EmployeeTable />
+    </div>
+  );
+};
+
+export default List;
