b69a2ca2dfc456599ac39d8c20b95dd1921f1fc9
Eduardo Marques <jose.eduardo.marques@accenture.com>
2019-05-17 15:34:45 -0300

    Scss adjusts in Masthead

--- src/components/Masthead/Masthead.js
+++ src/components/Masthead/Masthead.js
@@ -4,7 +4,7 @@ import "./Masthead.scss";
 const Masthead = (props) => {
   return (
     <div className={ props.class }>
-      <h1>{ props.title }</h1>
+      <h1 className="text-color">{ props.title }</h1>
     </div>
   )
 }
