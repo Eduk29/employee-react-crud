ddae41781f4e8705bf32a3dd761aa58ee646cb60
Eduardo Marques <edu.temarques@gmail.com>
2019-05-19 13:30:48 -0300

    Impl. imports of new table components

--- src/components/EmployeeTable/EmployeeTable.js
+++ src/components/EmployeeTable/EmployeeTable.js
@@ -1,61 +1,18 @@
 // React
-import React from "react";
+import React from 'react';
 
 // Material
-import { TableRow, TableCell, TableBody, Table, TableHead, TableFooter, TablePagination } from "@material-ui/core";
+import { Table } from '@material-ui/core';
+import EmployeeTableHead from '../EmployeeTableHead/EmployeeTableHead';
+import EmployeeTableRow from '../EmployeeTableRow';
 
 const EmployeeTable = props => {
   return (
     <div>
       <h2>EmployeeTable Component</h2>
-      <Table className="w-100">
-        <TableHead>
-          <TableRow>
-            <TableCell
-              className={"p-0 idColumn header-cell-text-size"}
-              colSpan="4"
-              align="center"
-            >
-              No.
-            </TableCell>
-            <TableCell
-              className={"p-0 nomeColumn header-cell-text-size"}
-              colSpan="6"
-              align="left"
-            >
-              Nome
-            </TableCell>
-            <TableCell
-              className={"p-0 dataColumn header-cell-text-size"}
-              colSpan="2"
-              align="left"
-            >
-              Data de Nascimento
-            </TableCell>
-            <TableCell
-              className={"p-0 cpfColumn header-cell-text-size"}
-              colSpan="2"
-              align="left"
-            >
-              CPF
-            </TableCell>
-            <TableCell
-              className={"p-0 rgColumn header-cell-text-size"}
-              colSpan="2"
-              align="left"
-            >
-              RG
-            </TableCell>
-            <TableCell
-              className={"p-0 telefoneColumn header-cell-text-size"}
-              colSpan="1"
-              align="left"
-            >
-              Telefone
-            </TableCell>
-            <TableCell />
-          </TableRow>
-        </TableHead>
+      <Table className='w-100'>
+        <EmployeeTableHead />
+        <EmployeeTableRow employees={ props.employees } pageInformations={ props.pageInformations } />
       </Table>
     </div>
   );
