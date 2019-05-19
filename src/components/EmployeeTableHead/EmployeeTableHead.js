6d07c51a579ffda870ffa06f15be79a333959014
Eduardo Marques <edu.temarques@gmail.com>
2019-05-19 13:30:17 -0300

    Impl. EmployeeTableHead Component

---
+++ src/components/EmployeeTableHead/EmployeeTableHead.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { TableRow, TableCell, TableHead } from '@material-ui/core';
+
+const EmployeeTableHead = () => {
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell
+          className={'p-0 idColumn header-cell-text-size'}
+          colSpan='4'
+          align='center'>
+          No.
+        </TableCell>
+        <TableCell
+          className={'p-0 nomeColumn header-cell-text-size'}
+          colSpan='6'
+          align='left'>
+          Nome
+        </TableCell>
+        <TableCell
+          className={'p-0 dataColumn header-cell-text-size'}
+          colSpan='2'
+          align='left'>
+          Data de Nascimento
+        </TableCell>
+        <TableCell
+          className={'p-0 cpfColumn header-cell-text-size'}
+          colSpan='2'
+          align='left'>
+          CPF
+        </TableCell>
+        <TableCell
+          className={'p-0 rgColumn header-cell-text-size'}
+          colSpan='2'
+          align='left'>
+          RG
+        </TableCell>
+        <TableCell
+          className={'p-0 telefoneColumn header-cell-text-size'}
+          colSpan='1'
+          align='left'>
+          Telefone
+        </TableCell>
+        <TableCell />
+      </TableRow>
+    </TableHead>
+  );
+};
+
+export default EmployeeTableHead;
