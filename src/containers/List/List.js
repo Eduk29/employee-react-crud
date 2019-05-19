7222bbe9e36d2fceaed0b1b5e581a065c9cd7fc0
Eduardo Marques <edu.temarques@gmail.com>
2019-05-19 13:31:22 -0300

    Impl. List Container to provide props

--- src/containers/List/List.js
+++ src/containers/List/List.js
@@ -1,13 +1,48 @@
-import React from "react";
-import EmployeeTable from "../../components/EmployeeTable/EmployeeTable";
+// React
+import React, { Component } from 'react';
 
-const List = () => {
-  return (
-    <div className="container">
-      <h2>Container List</h2>
-      <EmployeeTable />
-    </div>
-  );
+// Components
+import EmployeeTable from '../../components/EmployeeTable/EmployeeTable';
+
+// Service
+import * as EmployeeService from '../../services/EmployeeService';
+
+const initialState = {
+  employees: [],
+  pageInformations: {
+    pageIndex: 0,
+    pageSize: 10,
+    pageSizeOptions: [5, 10, 50, 100]
+  }
 };
 
+export class List extends Component {
+  // Constructor
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
+
+  // LifeCycle Methods
+  componentDidMount() {
+    this.getEmployeeList();
+  }
+
+  getEmployeeList() {
+    EmployeeService.getEmployeeList().then(data => {
+      let employees = { ...this.state.employees };
+      employees = data;
+      this.setState({ employees });
+    });
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <h2>Container List</h2>
+        <EmployeeTable employees={ this.state.employees } pageInformations={ this.state.pageInformations } />
+      </div>
+    );
+  }
+}
 export default List;
