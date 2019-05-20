c516862a0bd19bc46b825934171f9c942d215760
Eduardo Marques <jose.eduardo.marques@accenture.com>
2019-05-20 11:03:29 -0300

    Impl. Form Container and add in NewEmployee Page

---
+++ src/containers/Form/Form.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-2"> PhotoBox Component</div>
+        <div className="col-10"> EmployeeForm Component </div>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
--- src/pages/NewEmployee/NewEmployee.js
+++ src/pages/NewEmployee/NewEmployee.js
@@ -3,11 +3,13 @@ import React from 'react';
 
 // Containers
 import Header from './../../containers/Header/index';
+import Form from './../../containers/Form/index';
 
 const NewEmployee = () => {
   return (
     <div>
       <Header topMenuTitle="React CRUD Example APP" mastheadTitle="Create a new employee"/>
+      <Form />
     </div>
   )
 }
