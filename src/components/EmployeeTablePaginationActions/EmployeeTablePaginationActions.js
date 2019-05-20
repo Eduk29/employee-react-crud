7219b71fde8f7b1a3508d619ab066c996d479b73
Eduardo Marques <jose.eduardo.marques@accenture.com>
2019-05-20 15:59:21 -0300

    Some adjusts

--- src/components/EmployeeTablePaginationActions/EmployeeTablePaginationActions.js
+++ src/components/EmployeeTablePaginationActions/EmployeeTablePaginationActions.js
@@ -1,16 +1,13 @@
-import React from 'react';
+import React from "react";
 
 // Material
 import { IconButton } from "@material-ui/core";
-import FirstPageIcon from '@material-ui/icons/FirstPage';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
-import LastPageIcon from '@material-ui/icons/LastPage';
+import FirstPageIcon from "@material-ui/icons/FirstPage";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+import LastPageIcon from "@material-ui/icons/LastPage";
 
 const EmployeeTablePaginationActions = props => {
-
-  console.log('Props Pagination: ', props);
-  
   const handleFirstPageButtonClick = event => {
     props.onChangePage(event, 0);
   };
@@ -31,32 +28,36 @@ const EmployeeTablePaginationActions = props => {
   };
 
   return (
-    <div className={'d-flex flex-row'}>
+    <div className={"d-flex flex-row"}>
       <IconButton
         onClick={handleFirstPageButtonClick}
         disabled={props.page === 0}
-        aria-label='First Page'>
+        aria-label="First Page"
+      >
         <FirstPageIcon />
       </IconButton>
 
       <IconButton
         onClick={handleBackButtonClick}
         disabled={props.page === 0}
-        aria-label='Previous Page'>
+        aria-label="Previous Page"
+      >
         <KeyboardArrowLeft />
       </IconButton>
 
       <IconButton
         onClick={handleNextButtonClick}
         disabled={props.page >= Math.ceil(props.count / props.rowsPerPage) - 1}
-        aria-label='Next Page'>
+        aria-label="Next Page"
+      >
         <KeyboardArrowRight />
       </IconButton>
 
       <IconButton
         onClick={handleLastPageButtonClick}
         disabled={props.page >= Math.ceil(props.count / props.rowsPerPage) - 1}
-        aria-label='Last Page'>
+        aria-label="Last Page"
+      >
         <LastPageIcon />
       </IconButton>
     </div>
