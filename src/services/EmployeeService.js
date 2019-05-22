7c81bd788206d4516e8689a9e1eb158550797d43
Eduardo Marques <jose.eduardo.marques@accenture.com>
2019-05-22 19:55:37 -0300

    Impl. GET and PUT methods

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -10,11 +10,27 @@ export const getEmployeeList = async () => {
   return json;
 };
 
+export const getEmployee = async (id) => {
+  const response = await fetch(`${URL_API}/employees/${id}`);
+  const json = await response.json();
+  return json;
+};
+
 export const postEmployee = async newEmployee => {
   const response = await fetch(`${URL_API}/employees`, {
     method: "POST",
     headers: myHeaders,
     body: JSON.stringify(newEmployee)
+  });
+  const json = await response.json();
+  return json;
+};
+
+export const updateEmployee = async employee => {
+  const response = await fetch(`${URL_API}/employees/${employee.id}`, {
+    method: "PUT",
+    headers: myHeaders,
+    body: JSON.stringify(employee)
   });
   const json = await response.json();
   return json;
